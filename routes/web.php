1ca308ddb6cf1d5943ff06cfd084b95a5ae226d3
Everton Barros Jr <ebarrosjr@gmail.com>
2021-04-01 11:03:45 -0300

    Autenticação com Breeze

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\{
     PostController
 };
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
 Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
@@ -14,3 +11,13 @@ Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
 Route::get('/post/{id}', [PostController::class, 'show'])->name('posts.show');
 Route::get('/post/update/{id}', [PostController::class, 'edit'])->name('posts.update');
 Route::delete('/post/{id}', [PostController::class, 'destroy'])->name('posts.delete');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
+
+Route::get('/', function () {
+    return view('welcome');
+});
